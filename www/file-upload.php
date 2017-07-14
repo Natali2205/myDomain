787f0f8f43753f13f911811ab1eeeb1c94f8a3ff
Natalia <Natalia.Vasilets@gmail.com>
2017-07-14 22:49:04 +0300

    Upload files on server

---
+++ www/file-upload.php
@@ -0,0 +1,33 @@
+<?php
+//$n= $_POST['userfile[]'];
+//echo $_SERVER['HTTP_USER_AGENT'];
+echo $_FILES['userfile'];
+//echo 'Ви ввели такі дані: ';
+//echo gettype ($m);
+//if (is_int($n)){
+//	echo "$n , тип- integer";
+//}
+//if (is_string ($n)){
+//	echo "$n , тип- string";
+//}
+//if (is_float ($n)){
+//	echo "$n , тип- float";
+//}
+//if (is_bool ($n)){
+//	echo "$n , тип- bool";
+//}
+//$uploaddirec = '/myDomain/www/uploads/';
+
+$uploadfile = "uploads/".$_FILES['userfile']['name'];
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+   echo "Файл коректний і був успішно завантажений.\n";
+} else {
+    echo "Можлива атака за допомогою файлової загрузки!\n";
+}
+
+echo "Деяка інформація:";
+print_r($_FILES);
+
+
+?>
