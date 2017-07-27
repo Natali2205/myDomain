b2d77e34365da112d8f63ff6ae913a4a314d8008
Natalia <Natalia.Vasilets@gmail.com>
2017-07-27 17:10:38 +0300

    сommit

--- www/file-upload.php
+++ www/file-upload.php
@@ -2,45 +2,51 @@
 //$n= $_POST['userfile[]'];
 //echo $_SERVER['HTTP_USER_AGENT'];
 echo $_FILES['userfile'];
-//echo 'Ви ввели такі дані: ';
-//echo gettype ($m);
-//if (is_int($n)){
-//	echo "$n , тип- integer";
-//}
-//if (is_string ($n)){
-//	echo "$n , тип- string";
-//}
-//if (is_float ($n)){
-//	echo "$n , тип- float";
-//}
-//if (is_bool ($n)){
-//	echo "$n , тип- bool";
-//}
+
 //$uploaddirec = '/myDomain/www/uploads/';
 
 $uploadfile = "uploads/".$_FILES['userfile']['name'];
 echo '<pre>';
-if (!is_dir($uploads))
-   {mkdir($folder, 0777, true)}
+if (!is_dir($uploadfile)){
+   mkdir($folder, 0777, true);
+}
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-   echo "Файл коректний і був успішно завантажений.\n";
-    if($_FILES["userfile"]["size"] > 1024*3*1024)
+    echo "Файл коректний і був успішно завантажений.\n";
+      if($_FILES["userfile"]["size"] > 1024*3*1024)
    {
-     echo ("Розмір файла перебільшує 3 Мб");
+    echo ("Розмір файла перебільшує 3 Мб");
      exit;
    }
  }else {
    echo "Сталася помилка при завантаженні файлу.
 Деяка налагоджувальна інформація:\n";
    echo($_FILES);
+  
 
-} print "</pre>"; ?>
+if($picture_filesize == 0 ) {
+   exit ("Завантажений файл є пустим");
 }
-if (file_exists($uploadfile)) {
-    echo "Файл <b>$uploadfile</b> існує";}
-else {
-    echo "Файл <b>$uploadfile</b> НЕ існує";}
-echo "Деяка інформація:";
-print_r($uploadfile);
 
-?>
+$imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
+if($imageinfo['mime'] != 'image/png' && $imageinfo['mime'] != 'image/jpeg' && $imageinfo['mime'] != 'image/jpg' )
+exit ("Розширення зображення повинно бути jpg, jpeg, png ");
+ print "</pre>"; 
+
+if (file_exists($uploadfile)) {
+    echo "Файл <b>$uploadfile</b>Файл з такою назвою вже існує";}
+//else {
+//    echo "Файл <b>$uploadfile</b> НЕ існує";}
+echo "Деяка інформація:";
+
+$picture_filename = $_FILES['userfile']['name'];
+$picture_fileplace = $_FILES['userfile']['tmp_name'];
+$picture_filetype = $_FILES['userfile']['type'];
+$picture_filesize = $_FILES['userfile']['size'];
+ }
+ echo $picture_filename;
+ echo '<hr>';
+ echo $picture_fileplace;
+ echo '<hr>';
+ echo $picture_filetype;
+ echo '<hr>';
+ echo $picture_filesize;
