cc4dea7d0d783f1dab3bb487104d41702bfc05f9
Natalia <Natalia.Vasilets@gmail.com>
2017-07-31 13:31:36 +0300

    1

--- www/file-upload.php
+++ www/file-upload.php
@@ -5,59 +5,61 @@ echo $_FILES['userfile'];
 
 //$uploaddirec = '/myDomain/www/uploads/';
 
-$uploadfile = "uploads/".$_FILES['userfile']['name'];
+$uploadfile = "uploads/" . $_FILES['userfile']['name'];
+$folder = "uploads/";
 echo '<pre>';
-if (!is_dir($uploadfile)){
-   mkdir($folder, 0777, true);
+if (!is_dir($folder)&& mkdir($folder, 0777, true)){
+    echo("Існує папка");
+    exit;
 }
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "Файл коректний і був успішно завантажений.\n";
-      if($_FILES["userfile"]["size"] > 1024*3*1024)
-   {
-    echo ("Розмір файла перебільшує 3 Мб");
-     exit;
-   }
- }else {
-   echo "Сталася помилка при завантаженні файлу.
+    if ($_FILES["userfile"]["size"] > 1024 * 3 * 1024) {
+        echo("Розмір файла перебільшує 3 Мб");
+        exit;
+    }
+} else {
+    echo "Сталася помилка при завантаженні файлу.
 Деяка налагоджувальна інформація:\n";
-   echo($_FILES);
-  
+    echo($_FILES);
 
-if($picture_filesize == 0 ) {
-   exit ("Завантажений файл є пустим");
-}
 
-$imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
-if($imageinfo['mime'] != 'image/png' && $imageinfo['mime'] != 'image/jpeg' && $imageinfo['mime'] != 'image/jpg' )
-exit ("Розширення зображення повинно бути jpg, jpeg, png ");
- print "</pre>"; 
+    if ($picture_filesize == 0) {
+        exit ("Завантажений файл є пустим");
+    }
 
-if (file_exists($uploadfile)) {
-    echo "Файл <b>$uploadfile</b>Файл з такою назвою вже існує";}
+    $imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
+    if ($imageinfo['mime'] != 'image/png' && $imageinfo['mime'] != 'image/jpeg' && $imageinfo['mime'] != 'image/jpg')
+        exit ("Розширення зображення повинно бути jpg, jpeg, png ");
+    print "</pre>";
+
+    if (file_exists($uploadfile)) {
+        echo "Файл <b>$uploadfile</b>Файл з такою назвою вже існує";
+    }
 //else {
 //    echo "Файл <b>$uploadfile</b> НЕ існує";}
-echo "Деяка інформація:";
- }
- $extension_array=array('jpg', 'png','jpg');
- if (is_dir($uploadfile)){
-	 $files=scandir($uploadfile);
-	 for ($i=0; $i<count($files);$i++){
-		 if($files[$i]!='.'&&$files[$i]!='..'){
-			 echo "File Name-> $files[$i] <br>";
-		$file=pathinfo($files[$i];
-        $extension=$file['extension'];
+    echo "Деяка інформація:";
+}
+ $extension_array = array('jpg', 'png', 'jpg');
+ if (is_dir($folder)) {
+     $files = scandir($folder);
+     for ($i = 0; $i < count($files); $i++) {
+         if ($files[$i] != '.' && $files[$i] != '..') {
+             echo "File Name-> $files[$i]" . "" . " <br>";
+             $file = pathinfo($files[$i];
+             $extension = $file['extension'];
              echo "File extension-> $extension<br>";
-        if (in_array($extension, $extension_array)){
-			echo "<img src='$uploadfile$files[$i]' style='width:200px; height:200px;'><br>";
-			}
-		 }
-	 }
+             if (in_array($extension, $extension_array)) {
+                 echo "<img src='" . $folder . $files[$i] . "' style='width:200px; height:200px;'><br>";
+             }
+         }
+     }
  }	 
 $picture_filename = $_FILES['userfile']['name'];
 $picture_fileplace = $_FILES['userfile']['tmp_name'];
 $picture_filetype = $_FILES['userfile']['type'];
 $picture_filesize = $_FILES['userfile']['size'];
- }
+ 
  echo $picture_filename;
  echo '<hr>';
  echo $picture_fileplace;
---
+++ www/index.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta  content="text/html"; charset="utf-8"/>
+   <title>Визначення типу завантаження</title>
+   <style>
+   
+ 
+</style>
+ </head>
+ <body>
+   <form action="file-upload.php" method="post" enctype="multipart/form-data"></br>
+     <p>Ввести значення: </p> 
+	<input name="userfile" type="text"/>
+    <input type="submit" name="submit" value="Відправити"/><br><hr>
+     <p>Завантажити зображення: </p>
+	<input name="userfile" type="file" />
+    <input type="submit" value="Відправити" />
+   </form>
+ </body>
+</html>
+
