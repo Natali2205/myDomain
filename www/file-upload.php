54033aef2063172e272fa1ef81a099e122be0fe6
Natalia <Natalia.Vasilets@gmail.com>
2017-08-01 12:31:17 +0300

    utf-8

--- www/file-upload.php
+++ www/file-upload.php
@@ -1,4 +1,4 @@
-<?php
+п»ї<?php
 
 
 $uploadfile = "uploads/" . $_FILES['userfile']['name'];
@@ -7,48 +7,48 @@ $picture_filename = $_FILES['userfile']['name'];
 $picture_fileplace = $_FILES['userfile']['tmp_name'];
 $picture_filetype = $_FILES['userfile']['type'];
 $picture_filesize = $_FILES['userfile']['size'] / 1024;
-$image = $_FILES["userfile"]["name"]; /* зображення */
+$image = $_FILES["userfile"]["name"]; /* Р·РѕР±СЂР°Р¶РµРЅРЅСЏ  */
 $img = "uploads/" . $image;
 
 if (!is_dir($folder) && mkdir($folder, 0777, true)) {
-    echo("Існує папка");
+    echo("Р†СЃРЅСѓС” РїР°РїРєР°");
     exit;
 }
 
 if ($_FILES["userfile"]["size"] > 1024 * 3 * 1024) {
-    echo("Розмір файла перебільшує 3 Мб");
+    echo("Р РѕР·РјС–СЂ С„Р°Р№Р»Р° РїРµСЂРµР±С–Р»СЊС€СѓС” 3 РњР±");
     exit;
 }
 
 if ($_FILES["userfile"]["size"] == 0) {
-    exit("Завантажений файл є пустим");
+    exit("Р—Р°РІР°РЅС‚Р°Р¶РµРЅРёР№ С„Р°Р№Р» С” РїСѓСЃС‚РёРј");
 }
 
 if (mb_strlen($picture_filename, "UTF-8") > 100) {
-    exit("Імя файлу завелике, дозволяється не більше 100 символів");
+    exit("Р†РјСЏ С„Р°Р№Р»Сѓ Р·Р°РІРµР»РёРєРµ, РґРѕР·РІРѕР»СЏС”С‚СЊСЃСЏ РЅРµ Р±С–Р»СЊС€Рµ 100 СЃРёРјРІРѕР»С–РІ");
 }
 
 $blacklist = array(".php", ".phtml", ".php3", ".php4");
 foreach ($blacklist as $item) {
     if (preg_match("/$item\$/i", $_FILES['userfile']['name'])) {
-        echo "Заборонено завантаження php файлів";
+        echo "Р—Р°Р±РѕСЂРѕРЅРµРЅРѕ Р·Р°РІР°РЅС‚Р°Р¶РµРЅРЅСЏ php С„Р°Р№Р»С–РІ";
         exit;
     }
 };
 
 $imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
 if ($imageinfo['mime'] != 'image/png' && $imageinfo['mime'] != 'image/jpeg' && $imageinfo['mime'] != 'image/jpg')
-    exit("Розширення зображення повинно бути jpg, jpeg, png ");
+    exit("Р РѕР·С€РёСЂРµРЅРЅСЏ Р·РѕР±СЂР°Р¶РµРЅРЅСЏ РїРѕРІРёРЅРЅРѕ Р±СѓС‚Рё jpg, jpeg, png");
 print "</pre>";
 
 if (file_exists($uploadfile)) {
-    exit("Файл <b>$uploadfile</b>Файл з такою назвою вже існує");
+    exit(" Р¤Р°Р№Р»<b>$uploadfile</b>Р· С‚Р°РєРѕСЋ РЅР°Р·РІРѕСЋ РІР¶Рµ С–СЃРЅСѓС”");
 } else {
-    echo "Деяка інформація: ";
+    echo "Р”РµСЏРєР° С–РЅС„РѕСЂРјР°С†С–СЏ: ";
 }
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Файл коректний і був успішно завантажений.\n";
+    echo "Р¤Р°Р№Р» РєРѕСЂРµРєС‚РЅРёР№ С– Р±СѓРІ СѓСЃРїС–С€РЅРѕ Р·Р°РІР°РЅС‚Р°Р¶РµРЅРёР№.\n";
     $ext = substr($_FILES['userfile']['name'], 1 + strrpos($_FILES['userfile']['name'], "."));
     if (in_array($ext, array('jpeg', 'jpe', 'jpg')))
         $p = 'jpeg';
@@ -56,17 +56,17 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
         $p = 'gif';
     if (in_array($ext, array('png')))
         $p = 'png';
-    $filetype = "Файл має розширення: " . '.' . $p;
+    $filetype = "Р¤Р°Р№Р» РјР°С” СЂРѕР·С€РёСЂРµРЅРЅСЏ: " . '.' . $p;
 
 
     echo '<hr>' . '<img src= "' . $img . '">';
-    echo '<hr>' . "Назва картинки: " . $picture_filename;
+    echo '<hr>' . "РќР°Р·РІР° РєР°СЂС‚РёРЅРєРё: " . $picture_filename;
     echo '<hr>' . $filetype;
-    echo '<hr>' . "Розмір: " . $picture_filesize . " (Kb)";
-    echo '<hr>' . "Тип: " . $picture_filetype;
-    echo '<hr>' . "Шлях до файлу: " . $picture_fileplace;
+    echo '<hr>' . "Р РѕР·РјС–СЂ: " . $picture_filesize . " (Kb)";
+    echo '<hr>' . "РўРёРї: " . $picture_filetype;
+    echo '<hr>' . "РЁР»СЏС… РґРѕ С„Р°Р№Р»Сѓ: " . $picture_fileplace;
 } else {
-    echo "Сталася помилка при завантаженні файлу.\n";
+    echo "РЎС‚Р°Р»Р°СЃСЏ РїРѕРјРёР»РєР° РїСЂРё Р·Р°РІР°РЅС‚Р°Р¶РµРЅРЅС– С„Р°Р№Р»Сѓ.\n";
     echo($_FILES);
 }
 
--- www/index.php
+++ www/index.php
@@ -1,23 +1,23 @@
-<!DOCTYPE HTML>
+п»ї<!DOCTYPE HTML>
 <html>
- <head>
-  <meta  content="text/html"; charset="utf-8"/>
-   <title>Uploads Forms</title>
-    <link href="css/style.css" rel="stylesheet" type="text/css" >
-    
- </head>
-   <body>
-     <h1>Форми</h1>
-     <div>
-         <form action="file-upload.php" method="post" enctype="multipart/form-data"></br>
-       <!--  <h3>Введіть дані: </h3> 
-	   <input name="userfile" type="text"/>
-           <input type="submit" name="submit" value="Відправити"/><br><hr>-->
-           <h3>Завантаження зображення: </h3>
-	   <input name="userfile" type="file" />
-           <input type="submit" value="Відправити" />
-        </form>
-     </div>
-   </body>
+<head>
+    <meta content="text/html" ; charset="utf-8"/>
+    <title>Uploads Forms</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<h1>Р¤РѕСЂРјРё</h1>
+<div>
+    <form action="file-upload.php" method="post" enctype="multipart/form-data"></br>
+        <!--  <h3>file: </h3>
+        <input name="userfile" type="text"/>
+        <input type="submit" name="submit" value="send"/><br><hr>-->
+        <h3>Р—Р°РІР°РЅС‚Р°Р¶РµРЅРЅСЏ Р·РѕР±СЂР°Р¶РµРЅРЅСЏ: </h3>
+        <input name="userfile" type="file"/>
+        <input type="submit" value="Send"/>
+    </form>
+</div>
+</body>
 </html>
 
